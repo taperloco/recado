24a0313ba9aba4e628c573532a0fcf035e9388fe
taperloco <39673952+taperloco@users.noreply.github.com>
2025-05-19 23:21:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ import os
 if __name__ == '__main__':
 
     # Create and run Flask-Restful
-    app = create_app()
-
-    # Enable CORS with Authorization header exposed
-    CORS(app, supports_credentials=True, expose_headers=["Authorization"])
-    
+    app = create_app()   
     port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local dev
     app.run(host="0.0.0.0", port=port, debug=True)
 
