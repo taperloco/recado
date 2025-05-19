4228151ec38596f37206a3017a12f5d1ea9a93d0
taperloco <39673952+taperloco@users.noreply.github.com>
2025-05-19 19:28:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-mongo_URI = "mongodb+srv://darvilek:rP4BqX60cGHfgSSn@cluster0.pmmaiee.mongodb.net/, tls=True"
+mongo_URI = "mongodb+srv://darvilek:rP4BqX60cGHfgSSn@cluster0.pmmaiee.mongodb.net/?retryWrites=true&w=majority&tls=true"
 db_name = "Recado_Database"
 users_collection = "Users_collection"
 recados_collection = "Recados_collection"
