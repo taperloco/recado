126dd6f76c46ff4e8ac102aa56f462841f3fa473
taperloco <39673952+taperloco@users.noreply.github.com>
2025-05-20 19:00:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
 
     # Create and run Flask-Restful
     app = create_app()   
-    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local dev
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
     
